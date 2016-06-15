877839001400e436db752615c0ed36417bb921d0
Gordon Pendleton <gordon@internal.gandssecurityincorporated.com>
2016-06-15 15:12:56 -0400

    verify correct filename used if storage supports delete

--- informer/checker/storage.py
+++ informer/checker/storage.py
@@ -3,11 +3,13 @@
 """
 django informer checker for Storage
 """
+from uuid import uuid4
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
 from informer.checker.base import BaseInformer, InformerException
+from __builtin__ import False
 
 
 class StorageInformer(BaseInformer):
@@ -31,11 +33,22 @@ class StorageInformer(BaseInformer):
                 try:
                     self.storage.delete(filename)
                 except NotImplementedError:
-                    pass
+                    verify_filename = False
+                else:
+                    verify_filename = True
 
             # Save data.
-            content = ContentFile('File used by StorageInformer checking.')
+            data = uuid4().hex
+            content = ContentFile(data)
             path = self.storage.save(filename, content)
+
+            # Check saved file name
+            if verify_filename:
+                if path != filename:
+                    raise InformerException(
+                    ('Invalid filename returned after writing to your '
+                     '%s storage.') %
+                    self.storage.__class__.__name__)
 
             # Check properties.
             try:
@@ -62,6 +75,12 @@ class StorageInformer(BaseInformer):
                 self.storage.created_time(path)
             except NotImplementedError:
                 pass
+            
+            # Check written data matches read data
+            if self.storage.open(path).read() != data:
+                raise InformerException(
+                'Invalid data read after writing to your %s storage.' %
+                self.storage.__class__.__name__)
 
             # And remove file.
             try:
